e1cb24efe243724d290e47f3aad956232ac677e8
alijhnm <ajahanmm@gmail.com>
2019-08-03 18:18:47 +0430

    modify trigger creation page in admin panel. Now the trigger is registered in admin panel.

--- django_signal_notifier/admin.py
+++ django_signal_notifier/admin.py
@@ -1,8 +1,42 @@
 from django.contrib import admin
+from django_signal_notifier.models import *
 
-from django_signal_notifier import models
+admin.site.register(Subscription)
+admin.site.register(Backend)
+admin.site.register(TestModel)
+admin.site.register(TestModel2)
+admin.site.register(BasicUser)
+admin.site.register(Template)
 
-admin.site.register(models.Subscription)
-admin.site.register(models.Trigger)
-admin.site.register(models.Backend)
-admin.site.register(models.TestModel)
+
+class TriggerAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        data = form.cleaned_data
+        verb = data.get("verb")
+        action_object_id = data.get("action_object_id")
+        action_object_content_type = data.get("action_object_content_type")
+        actor_object_content_type = data.get('actor_object_content_type')
+        actor_object_id = data.get('actor_object_id')
+        target = data.get("target")
+
+        if actor_object_id is None:
+            action_object = action_object_content_type
+        else:
+            if action_object_id != "":
+                action_object = action_object_content_type.model_class().objects.filter(pk=int(action_object_id))
+            else:
+                action_object = action_object_content_type
+
+        if actor_object_content_type is None:
+            actor_object = None
+        else:
+            if actor_object_id != "":
+                actor_object = actor_object_content_type.model_class().objects.filter(pk=int(actor_object_id))
+            else:
+                actor_object = actor_object_content_type
+
+        Trigger.register_trigger(verb_name=verb, action_object=action_object, actor=actor_object, target=target)
+
+
+admin.site.register(Trigger, TriggerAdmin)
