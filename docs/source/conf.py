761081d3fba786e0833fa7bde83e71a75093de84
hadiazad <m.h.azaddel@gmail.com>
2020-01-21 16:53:10 +0330

    readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,4 +55,6 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-master_doc = 'source/index'
+
+# The master toctree document.
+master_doc = 'index'
