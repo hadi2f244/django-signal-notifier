ffc95f213f1adb4f11f5e86aa743140663d9d7c1
hadiazad <m.h.azaddel@gmail.com>
2020-02-03 17:58:05 +0330

    readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,7 +73,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'source/index'
 
 # -- Project information -----------------------------------------------------
 
